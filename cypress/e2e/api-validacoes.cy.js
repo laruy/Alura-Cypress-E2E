addd3be166777f54b9ddcd24b4787d282b129523
Lais Maruyama <laismaruyama@outlook.com>
2025-11-03 12:10:45 -0300

    12 - Faça como eu fiz: implementando boas práticas

--- cypress/e2e/api-validacoes.cy.js
+++ cypress/e2e/api-validacoes.cy.js
@@ -1,11 +1,10 @@
 describe('API AdoPet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZjFlZDkzZS1jMjljLTQ2MGUtYmJkOC1mYmYxYWE4MjU4MGYiLCJhZG9wdGVyTmFtZSI6IkxhaXMiLCJpYXQiOjE3NTkzMzIyMjAsImV4cCI6MTc1OTU5MTQyMH0.aYyXWmbo9RvT-SN2WuaIZnHxhf5Yp960ShfYlaWdGww`
 
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/af1ed93e-c29c-460e-bbd8-fbf1aa82580f',
-            headers: {authorization}
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
@@ -17,7 +16,7 @@ describe('API AdoPet', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/af1ed93e-c29c-460e-bbd8-fbf1aa82580f',
-            headers: {authorization}
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).to.have.property('perfil')
